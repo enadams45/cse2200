173fa1dafe3b5c30717f9429094f47a206eeade4
enadams45 <enadams45@gmail.com>
2024-10-29 12:16:32 +0600

    Update ToInvisible.java

--- ToInvisible.java
+++ ToInvisible.java
@@ -2,20 +2,41 @@ public class ToInvisible {
     public static void main(String[] args) {
         String msg = "Hello";
         String invisibleMsg = makeInvisible(msg);
-        System.out.println(invisibleMsg);
+        System.out.println("Encoded (Invisible): " + invisibleMsg);
+        
+        String decodedMsg = decodeInvisible(invisibleMsg);
+        System.out.println("Decoded: " + decodedMsg);
     }
 
     static String makeInvisible(String msg) {
         StringBuilder invisibleMsg = new StringBuilder();
-        for(char c:msg.toCharArray()) {
+        for (char c : msg.toCharArray()) {
             String binString = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
-            for(char binChar:binString.toCharArray()) {
-                if(binChar == '1') {
+            for (char bit : binString.toCharArray()) {
+                if (bit == '1') {
                     invisibleMsg.append(' ');
+                } else {
+                    invisibleMsg.append('\t');
                 }
-                else invisibleMsg.append('\t');
             }
         }
         return invisibleMsg.toString();
+    }
+
+    static String decodeInvisible(String invisibleMsg) {
+        StringBuilder decodedMsg = new StringBuilder();
+        for (int i = 0; i < invisibleMsg.length(); i += 8) {
+            StringBuilder binString = new StringBuilder();
+            for (int j = i; j < i + 8; j++) {
+                if (invisibleMsg.charAt(j) == ' ') {
+                    binString.append('1');
+                } else if (invisibleMsg.charAt(j) == '\t') {
+                    binString.append('0');
+                }
+            }
+            int charCode = Integer.parseInt(binString.toString(), 2);
+            decodedMsg.append((char) charCode);
+        }
+        return decodedMsg.toString();
     }
 }
